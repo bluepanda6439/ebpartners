f439bd0d4ccdbe72b5e720890821acd0519a9080
Kasia <kasia@MacBook-Air-Kasia.local>
2026-07-05 20:19:30 +0200

    Refine contact section and header CTA

---
+++ lib/contact-data.ts
@@ -0,0 +1,35 @@
+export const contactEmail = "kontakt@ebpartners.pl";
+
+export const contactPhones = [
+  {
+    name: "Ewelina Bojczuk",
+    label: "Main contact",
+    phone: "+48 571 536 626",
+    href: "tel:+48571536626",
+  },
+  {
+    name: "Benna Hanyani",
+    label: "B2B & international cases",
+    phone: "+48 503 325 049",
+    href: "tel:+48503325049",
+  },
+];
+
+export const contactLocation = {
+  label: "Location",
+  name: "MONO LEGALIZATION",
+  href: "https://www.google.com/maps/search/?api=1&query=MONO%20LEGALIZATION",
+  embedSrc:
+    "https://www.google.com/maps?q=MONO%20LEGALIZATION&output=embed",
+};
+
+export const socialLinks = [
+  {
+    label: "Facebook",
+    href: "#",
+  },
+  {
+    label: "Instagram",
+    href: "#",
+  },
+];
